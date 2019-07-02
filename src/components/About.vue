27b959c877ee2a954c5bd26bcb5f82d2133a1d8c
pkanaganti <pkanaganti@unomaha.edu>
2019-07-01 22:46:44 -0500

    vue project

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is {{ their_name }}'s To Do list developed in Vue.Js.</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    }
+  }
+}
+</script>
